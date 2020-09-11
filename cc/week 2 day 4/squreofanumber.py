d06d2911f739dd3470997cff3c21fef819bb82b6
Rinkesh <rinkeshravi2@gmail.com>
2020-09-11 21:19:06 +0530

    cc of week 2 day 4 is completed

---
+++ cc/week 2 day 4/squreofanumber.py
@@ -0,0 +1,19 @@
+#Write a program which will print square of a number.
+# Some random values
+valueA = 4
+valueB = 2
+valueC = 6
+
+
+# Square each value
+squareA = valueA ** 2
+squareB = valueB ** 2
+squareC = valueC ** 2
+
+
+# Output each value squared
+print(valueA, "square is =", squareA)
+print(valueB, "square is =", squareB)
+print(valueC, "square is =", squareC)
+
+
