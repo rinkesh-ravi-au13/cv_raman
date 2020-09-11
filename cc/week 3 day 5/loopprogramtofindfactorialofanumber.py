6cab109f965c05faf8bf96bf4b1971fb48da4603
Rinkesh <rinkeshravi2@gmail.com>
2020-09-11 18:13:11 +0530

    cc of week 3 day 2 question 1 is done

---
+++ cc/week 3 day 5/loopprogramtofindfactorialofanumber.py
@@ -0,0 +1,9 @@
+#Question 1: Write a loop to find the factorial of any number
+#loop program to find factorial of a number
+num = int(input("Enter any  number: "))
+fac = 1
+
+for i in range (1, num+1):
+    fac = fac * i
+    print ("Factorial of the number %d is %d" %(num, fac))
+  
