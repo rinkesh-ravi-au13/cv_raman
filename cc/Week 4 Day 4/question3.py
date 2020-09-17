5e5183a0849f03cfb06c90b8e48a88f2d8a9b952
Rinkesh <rinkeshravi2@gmail.com>
2020-09-17 20:49:47 +0530

    CC Of Week $ Day 4 Question #3 Is Not Done

---
+++ cc/Week 4 Day 4/question3.py
@@ -0,0 +1,6 @@
+#Write a Python program to create a dictionary from a string. Note: Track thecount of the letters from
+#  the string. Sample string : 'w3resource' Expected output:
+#{'3': 1, 's': 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}
+
+# Program not working now 
+#will attempt it later 
