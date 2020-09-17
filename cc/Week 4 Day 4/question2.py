b9b56226cb4fdb306fb55928ef1b00e61fae331e
Rinkesh <rinkeshravi2@gmail.com>
2020-09-17 20:07:26 +0530

    CC Of Week 4 Day 4 Question 2 Is Done

---
+++ cc/Week 4 Day 4/question2.py
@@ -0,0 +1,16 @@
+#Write a Python script to generate and print a dictionary that contains a number(between 1 and n) 
+# in the form (x, x*x)
+
+a = int(input("Input Any Number :- "))
+d2= dict() 
+
+for x in range( 1, a+1 ): 
+    d2 [x] = x * x 
+    print(d2) 
+
+# output :-
+#Input Any Number :- 4
+#{1: 1}
+#{1: 1, 2: 4}
+#{1: 1, 2: 4, 3: 9}
+#{1: 1, 2: 4, 3: 9, 4: 16}
