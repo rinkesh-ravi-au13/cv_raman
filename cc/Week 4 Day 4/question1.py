aeff02aaf7ff63361a211adab3dbcf70b11e0a82
Rinkesh <rinkeshravi2@gmail.com>
2020-09-17 19:44:41 +0530

    CC of week 4 Day 4 Question 1 Is Done

---
+++ cc/Week 4 Day 4/question1.py
@@ -0,0 +1,11 @@
+#)Write a Python program to iterate over dictionaries using for loops.
+
+d1={' Name :- ': ' Rinkesh Kumar Ravi ', ' Age :- ': 24, ' Marks Obtained :- ': '90 Out Of 100' }
+for a,b in d1.items():
+    print (a,b)
+
+# Output :-
+# Name :-   Rinkesh Kumar Ravi 
+# Age :-  24
+#Marks Obtained :-  90 Out Of 100
+
