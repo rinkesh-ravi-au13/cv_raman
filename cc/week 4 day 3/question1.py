33f39e1ba259411ca8a672746b380e82b7a9af3b
Rinkesh <rinkeshravi2@gmail.com>
2020-09-18 11:33:59 +0530

    CC of week 4 day 3 question 1 is done

---
+++ cc/week 4 day 3/question1.py
@@ -0,0 +1,7 @@
+#Q1. Write a Python program to sum all the items in a list.
+#Eg [1,2,3,4] = 10
+total = 0
+list1 = [40, 13, 44 , 51, 34 , 23 ]
+for items  in range(0, len(list1)): 
+    total = total + list1 [ items ]
+    print("Sum of all items in given list: ", total) 
