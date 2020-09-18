695bf7aff59955bfc0dc34a43d45f6d94f9dc0b0
Rinkesh <rinkeshravi2@gmail.com>
2020-09-18 11:42:02 +0530

    CC of week 4 day 3 question 2 is done

---
+++ cc/week 4 day 3/question2.py
@@ -0,0 +1,6 @@
+#Q2. Write a Python program to get the largest number from a list.
+#[1,2,33,4] = largest is 33
+
+list1 = [15, 55, 98,76, 16,54]
+list1.sort()
+print("Largest Number In The List Is :- ", list1[-1])
