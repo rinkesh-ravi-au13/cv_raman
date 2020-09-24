cf736a9596e9186507a83a5e03708bf17b9a40c9
Rinkesh <rinkeshravi2@gmail.com>
2020-09-24 23:28:07 +0530

    cc of week 5 day 4  is done

---
+++ cc/week 5 day 4/cc.py
@@ -0,0 +1,31 @@
+#question 1. what is the time complexity of the following code ?
+
+# a=0, i = N
+# while i > 0
+#    a += i
+#    i /= 2
+##complexity of code :-
+
+### log2N
+ 
+ 
+ #question 2 :-
+ 
+# i = n/2
+ #while i < n :
+ #    j = 2
+ #    while j < n :
+ #        j *= 2
+ ##complexity of code :-
+ 
+ ### O(n^2)
+ 
+ #count =m0 ;
+ #i = N
+ #while i>0:
+ #    while j<i:
+ #    i/=2
+
+ ##complexity of code :-
+
+ ###O(nlog2n)
