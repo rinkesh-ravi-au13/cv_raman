abdf820b2488dfa8e8ac351d252c8aea7b3cc89f
Rinkesh <rinkeshravi2@gmail.com>
2020-10-07 23:01:09 +0530

    question2 is done

---
+++ Assignment/week 7 day 1/question2.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+            else:
+                if target<nums[i] and target>nums[i-1]:
+                    return i
+                elif target>nums[-1]:
+                    return len(nums)
+                elif target<nums[0]:
+                    return 0
