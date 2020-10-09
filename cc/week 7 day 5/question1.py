58846b1a5eb5f1ed2de018679da02fda8fa2ee34
Rinkesh <rinkeshravi2@gmail.com>
2020-10-09 23:43:13 +0530

    question 1 is done

---
+++ cc/week 7 day 5/question1.py
@@ -0,0 +1,22 @@
+#Question :- https://leetcode.com/problems/merge-sorted-array/
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        m -= 1
+        n -= 1
+        t = m+n+1
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[t] = nums1[m]
+                m -= 1
+            else:
+                nums1[t] = nums2[n]
+                n -= 1
+            t -= 1
+        while m >= 0:
+            nums1[t] = nums1[m]
+            m -= 1
+            t -= 1
+        while n >= 0:
+            nums1[t] = nums2[n]
+            n -= 1
